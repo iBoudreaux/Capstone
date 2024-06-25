119d3386e3426b9033fe5de66ab7849d1e3fab8e
Isabella Boudreaux <iboudreaux@student.neumont.edu>
2024-06-25 13:14:21 -0600

    Added pages folder with welcome page and added backrgound

---
+++ signlanguageapp/lib/Pages/welcomepages/welcomepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade300,
+    );
+  }
+}
+
